8ae8b4257c2a0794cb7ddfafee1187a2b8bc5823
Reiland Eubank <reilandeubank@proton.me>
2025-09-20 14:04:12 -0500

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,275 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      (import "${home-manager}/nixos")
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "nfs" ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  fileSystems."mnt/media/media-share" = {
+    device = "192.168.0.100:/mnt/media/media-share";
+    fsType = "nfs";
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+# Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  programs.zsh.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.reilandeubank = {
+    isNormalUser = true;
+    description = "Reiland Eubank";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+    shell = pkgs.zsh;
+  };
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  home-manager.users.reilandeubank = {
+    home.stateVersion = "25.05";
+    home.packages = with pkgs.gnomeExtensions; [
+      appindicator
+      dash-to-dock
+      system-monitor
+      pop-shell
+    ];
+    dconf = {
+      enable = true;
+      settings."org/gnome/shell" = {
+        disable-user-extensions = false;
+	enabled-extensions = with pkgs.gnomeExtensions; [
+	  appindicator.extensionUuid
+	  dash-to-dock.extensionUuid
+	  system-monitor.extensionUuid
+	  pop-shell.extensionUuid
+	];
+      };
+      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    };
+
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+
+      oh-my-zsh = {
+        enable = true;
+	theme = "robbyrussell";
+	plugins = [
+	  "git"
+	  "sudo"
+	  "history"
+	  "fzf"
+	];
+      };
+      initContent = ''
+        export EDITOR=nvim
+	alias sd="cd ~ && cd \$(find * -type d | fzf)"
+      '';
+    };
+
+    programs.kitty = {
+      enable = true;
+      font = {
+        name = "JetBrainsMonoNF";
+	size = 11.0;
+      };
+
+      settings = {
+      # Font styles
+	    bold_font        = "JetBrainsMonoNF-Bold";
+	    italic_font      = "JetBrainsMonoNF-Italic";
+	    bold_italic_font = "JetBrainsMonoNF-BoldItalic";
+	    disable_ligatures = "never";
+
+	    # Transparency
+	    background_opacity = "0.9";
+
+	    # Basic colors
+	    foreground = "#CDD6F4";
+	    background = "#1E1E2E";
+	    selection_foreground = "#1E1E2E";
+	    selection_background = "#F5E0DC";
+
+	    # Cursor
+	    cursor            = "#F5E0DC";
+	    cursor_text_color = "#1E1E2E";
+
+	    # URL underline color
+	    url_color = "#F5E0DC";
+
+	    # Window borders
+	    active_border_color   = "#B4BEFE";
+	    inactive_border_color = "#6C7086";
+	    bell_border_color     = "#F9E2AF";
+
+	    # Titlebars
+	    wayland_titlebar_color = "#1E1E2E";
+
+	    # Tabs
+	    active_tab_foreground   = "#11111B";
+	    active_tab_background   = "#CBA6F7";
+	    inactive_tab_foreground = "#CDD6F4";
+	    inactive_tab_background = "#181825";
+	    tab_bar_background      = "#11111B";
+
+	    # Marks
+	    mark1_foreground = "#1E1E2E";
+	    mark1_background = "#B4BEFE";
+	    mark2_foreground = "#1E1E2E";
+	    mark2_background = "#CBA6F7";
+	    mark3_foreground = "#1E1E2E";
+	    mark3_background = "#74C7EC";
+
+	    # 16 terminal colors
+	    color0  = "#45475A";
+	    color8  = "#585B70";
+	    color1  = "#F38BA8";
+	    color9  = "#F38BA8";
+	    color2  = "#A6E3A1";
+	    color10 = "#A6E3A1";
+	    color3  = "#F9E2AF";
+	    color11 = "#F9E2AF";
+	    color4  = "#89B4FA";
+	    color12 = "#89B4FA";
+	    color5  = "#F5C2E7";
+	    color13 = "#F5C2E7";
+	    color6  = "#94E2D5";
+	    color14 = "#94E2D5";
+	    color7  = "#BAC2DE";
+	    color15 = "#A6ADC8";
+      };
+    };
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+	neovim
+	# kitty
+	steam
+	vlc
+	vscode-fhs
+	htop
+	fastfetch
+	localsend
+	discord
+	git
+	gh
+	pop-icon-theme
+	fzf
+  	wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
